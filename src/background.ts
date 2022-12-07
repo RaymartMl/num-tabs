de775ada7ceb30785fe53e6d885d724aa4617491
Raymart Balaguer <38749661+RaymartMl@users.noreply.github.com>
2022-12-08 01:16:52 +0800

    feat: convert code from numbered-tabs to manifest v3

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,61 @@
-let active = false;
+type Tab = chrome.tabs.Tab;
 
-function makeOrange(color: string): void {
-    document.body.style.backgroundColor = color;
+const tabs = chrome.tabs;
+tabs.onCreated.addListener(() => updateAllTabs());
+tabs.onUpdated.addListener(() => updateAllTabs());
+tabs.onMoved.addListener(() => updateAllTabs());
+tabs.onDetached.addListener(() => updateAllTabs());
+tabs.onAttached.addListener(() => updateAllTabs());
+tabs.onRemoved.addListener(() => updateAllTabs());
+
+function updateTab(tab: Tab) {
+  const { id, index, url, title } = tab;
+  if (url.startsWith("chrome://")) {
+    return;
+  }
+
+  try {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: id },
+        func: addTabPrefix,
+        args: [index, title],
+      })
+      .then();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-chrome.action.onClicked.addListener((tab) => {
-    active = !active;
-    const color = active ? 'orange' : 'white';
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id ? tab.id : -1},
-        func: makeOrange,
-        args: [color]
-    }).then();
-});
+function addTabPrefix(index: number, title: string) {
+  const prefixRegEx = /^[0-9-]+. /g;
+  const num = index + 1;
+
+  let newPrefix = `${num}. `;
+
+  console.log({ newPrefix });
+  const hasPrefix = prefixRegEx.exec(title);
+  if (hasPrefix[0] === newPrefix) {
+    return;
+  } else if (hasPrefix) {
+    // tab moved
+    newPrefix += title.split(prefixRegEx)[1];
+  } else {
+    // new tab
+    newPrefix += title;
+  }
+
+  document.title = `${newPrefix}`;
+}
+
+function getAllTabs(cb: (tabs: Tab[]) => void) {
+  chrome.tabs.query({}, cb);
+}
+
+function updateAllTabs() {
+  const cb = (tabs: Tab[]) => {
+    console.log("what is this");
+    tabs.forEach((tab) => updateTab(tab));
+  };
+  getAllTabs(cb);
+}
