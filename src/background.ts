0c47ba5b1d8200361032bf905bc1f0529717396f
Raymart Balaguer <38749661+RaymartMl@users.noreply.github.com>
2022-12-08 02:10:34 +0800

    fix: executeScript not working

--- src/background.ts
+++ src/background.ts
@@ -1,47 +1,45 @@
 type Tab = chrome.tabs.Tab;
 
-const tabs = chrome.tabs;
-tabs.onCreated.addListener(() => updateAllTabs());
-tabs.onUpdated.addListener(() => updateAllTabs());
-tabs.onMoved.addListener(() => updateAllTabs());
-tabs.onDetached.addListener(() => updateAllTabs());
-tabs.onAttached.addListener(() => updateAllTabs());
-tabs.onRemoved.addListener(() => updateAllTabs());
+chrome.runtime.onStartup.addListener(updateAllTabs);
+chrome.tabs.onCreated.addListener(updateAllTabs);
+chrome.tabs.onUpdated.addListener(updateAllTabs);
+chrome.tabs.onMoved.addListener(updateAllTabs);
+chrome.tabs.onDetached.addListener(updateAllTabs);
+chrome.tabs.onAttached.addListener(updateAllTabs);
+chrome.tabs.onRemoved.addListener(updateAllTabs);
 
 function updateTab(tab: Tab) {
   const { id, index, url, title } = tab;
+
   if (url.startsWith("chrome://")) {
     return;
   }
 
-  try {
-    chrome.scripting
-      .executeScript({
-        target: { tabId: id },
-        func: addTabPrefix,
-        args: [index, title],
-      })
-      .then();
-  } catch (e) {
-    console.error(e);
-  }
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: id, allFrames: true },
+      func: addTabPrefix,
+      args: [index, title],
+    },
+    (a) => {
+      if (!chrome.runtime.lastError) {
+        return a;
+      }
+    },
+  );
 }
 
 function addTabPrefix(index: number, title: string) {
   const prefixRegEx = /^[0-9-]+. /g;
   const num = index + 1;
-
   let newPrefix = `${num}. `;
 
-  console.log({ newPrefix });
   const hasPrefix = prefixRegEx.exec(title);
-  if (hasPrefix[0] === newPrefix) {
+  if (hasPrefix && hasPrefix[0] === newPrefix) {
     return;
   } else if (hasPrefix) {
-    // tab moved
     newPrefix += title.split(prefixRegEx)[1];
   } else {
-    // new tab
     newPrefix += title;
   }
 
@@ -54,7 +52,6 @@ function getAllTabs(cb: (tabs: Tab[]) => void) {
 
 function updateAllTabs() {
   const cb = (tabs: Tab[]) => {
-    console.log("what is this");
     tabs.forEach((tab) => updateTab(tab));
   };
   getAllTabs(cb);
